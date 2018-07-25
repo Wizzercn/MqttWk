e6ccc5292fe710f447bdb452945d8a093207410a
Wizzercn <wizzer@qq.com>
2018-07-25 09:08:40 +0800

    ..

--- mqtt-store/src/main/java/cn/wizzer/iot/mqtt/server/store/session/SessionStoreService.java
+++ mqtt-store/src/main/java/cn/wizzer/iot/mqtt/server/store/session/SessionStoreService.java
@@ -6,35 +6,46 @@ package cn.wizzer.iot.mqtt.server.store.session;
 
 import cn.wizzer.iot.mqtt.server.common.session.ISessionStoreService;
 import cn.wizzer.iot.mqtt.server.common.session.SessionStore;
+import org.nutz.aop.interceptor.async.Async;
+import org.nutz.integration.jedis.RedisService;
+import org.nutz.ioc.impl.PropertiesProxy;
+import org.nutz.ioc.loader.annotation.Inject;
 import org.nutz.ioc.loader.annotation.IocBean;
-
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import org.nutz.lang.Lang;
 
 /**
  * 会话存储服务
  */
 @IocBean
 public class SessionStoreService implements ISessionStoreService {
-    private Map<String, SessionStore> sessionCache = new ConcurrentHashMap<String, SessionStore>();
+    private final static String CACHE_PRE = "mqttwk:session:";
+    @Inject
+    private RedisService redisService;
+    @Inject
+    private PropertiesProxy conf;
 
     @Override
     public void put(String clientId, SessionStore sessionStore) {
-        sessionCache.put(clientId, sessionStore);
+        redisService.set((CACHE_PRE + clientId).getBytes(), Lang.toBytes(sessionStore));
     }
+
 
     @Override
     public SessionStore get(String clientId) {
-        return sessionCache.get(clientId);
+        byte[] obj = redisService.get((CACHE_PRE + clientId).getBytes());
+        if (obj != null)
+            return Lang.fromBytes(obj, SessionStore.class);
+        return null;
     }
 
     @Override
     public boolean containsKey(String clientId) {
-        return sessionCache.containsKey(clientId);
+        return !redisService.keys((CACHE_PRE + clientId).getBytes()).isEmpty();
     }
 
     @Override
+    @Async
     public void remove(String clientId) {
-        sessionCache.remove(clientId);
+        redisService.del((CACHE_PRE + clientId).getBytes());
     }
 }
