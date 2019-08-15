c6a5cef7fe90a54c28cb6c91f1f3c0cb1c8042d2
Wizzercn <wizzer@qq.com>
2019-08-15 11:47:18 +0800

    fixed: #7 尝试修复

--- mqtt-store/src/main/java/cn/wizzer/iot/mqtt/server/store/message/MessageIdService.java
+++ mqtt-store/src/main/java/cn/wizzer/iot/mqtt/server/store/message/MessageIdService.java
@@ -4,12 +4,15 @@ import cn.wizzer.iot.mqtt.server.common.message.IMessageIdService;
 import org.nutz.integration.jedis.RedisService;
 import org.nutz.ioc.loader.annotation.Inject;
 import org.nutz.ioc.loader.annotation.IocBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by wizzer on 2018
  */
 @IocBean(create = "init")
 public class MessageIdService implements IMessageIdService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MessageIdService.class);
 
     @Inject
     private RedisService redisService;
@@ -17,12 +20,14 @@ public class MessageIdService implements IMessageIdService {
     @Override
     public int getNextMessageId() {
         try {
-            int nextMsgId = (int) (redisService.incr("mqttwk:messageid:num") % 65536);
-            if (nextMsgId == 0)
-                return this.getNextMessageId();
-            return nextMsgId;
+            while (true) {
+                int nextMsgId = (int) (redisService.incr("mqttwk:messageid:num") % 65536);
+                if (nextMsgId > 0) {
+                    return nextMsgId;
+                }
+            }
         } catch (Exception e) {
-            e.printStackTrace();
+            LOGGER.error(e.getMessage(), e);
         }
         return 0;
     }
